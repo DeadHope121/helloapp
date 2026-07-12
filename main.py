6b35b5cbcc83a830f14f028c8e6be47e8bc5d00b
black <black@example.com>
2026-07-13 02:15:00 +0500

    Add greetBlack

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ def greet():
 def greetWhite():
     print("hello white")
 
-def greetWhite():
-    print("hello white")
+def greetBlack():
+    print("hello black")
 
 if __name__ == "__main__":
     greet()
+    greetWhite()
+    greetBlack()
